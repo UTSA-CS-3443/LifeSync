6cee7ee2cbf773fd6cd2eb57efee8acbc3c1325d
dylan <dylanf1002d@gmail.com>
2024-07-24 17:51:08 -0500

    Adding thirdactivty as an activity this time

--- app/src/main/java/edu/utsa/cs3443/lifesync/ThirdActivity.java
+++ app/src/main/java/edu/utsa/cs3443/lifesync/ThirdActivity.java
@@ -14,7 +14,7 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_third);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -22,4 +22,3 @@ public class ThirdActivity extends AppCompatActivity {
         });
     }
 }
-//testing
