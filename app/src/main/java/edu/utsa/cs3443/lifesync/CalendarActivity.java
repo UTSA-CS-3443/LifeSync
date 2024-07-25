18ecfd7446bc3c96204e4f1ca927e890c7659087
Jaime1108 <166854304+Jaime1108@users.noreply.github.com>
2024-07-25 16:18:48 -0500

    model class change

---
+++ app/src/main/java/edu/utsa/cs3443/lifesync/CalendarActivity.java
@@ -0,0 +1,40 @@
+package edu.utsa.cs3443.lifesync;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Calendar;
+
+public class CalendarActivity extends AppCompatActivity {
+    CalendarView calendarView;
+    Calendar calendar
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_calendar);
+        calendarView = findViewById(R.id.calendarView);
+        calendar = Calendar.getInstance();
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                Toast.makeText(CalendarActivity.this,day +"/" +month +"/"+year , Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    public void setDate(int day, int month, int year){
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, year);
+        calendar.set(Calendar.YEAR, year);
+
+    }
+}
--- app/src/main/java/edu/utsa/cs3443/lifesync/model/Task.java
+++ app/src/main/java/edu/utsa/cs3443/lifesync/model/Task.java
@@ -2,18 +2,15 @@ package edu.utsa.cs3443.lifesync.model;
 
 import java.util.ArrayList;
 public class Task extends Widget {
-    private String reminderDayBefore;
-    private String reminderTimeBefore;
+
     private String repeatDay;
     private String repeatTime;
     private long startTime;
     private long endTime;
 
-    public Task(String id, String title, String color, String description,
-                String reminderDayBefore, String reminderTimeBefore, String repeatDay, String repeatTime, long startTime, long endTime) {
+    public Task(String id, String title, String color, String description, String repeatDay, String repeatTime, long startTime, long endTime) {
         super(id, title, color, description);
-        this.reminderDayBefore = reminderDayBefore;
-        this.reminderTimeBefore = reminderTimeBefore;
+
         this.repeatDay = repeatDay;
         this.repeatTime = repeatTime;
         this.startTime = startTime;
@@ -21,21 +18,7 @@ public class Task extends Widget {
     }
 
     // Getters and Setters
-    public String getReminderDayBefore() {
-        return reminderDayBefore;
-    }
 
-    public void setReminderDayBefore(String reminderDayBefore) {
-        this.reminderDayBefore = reminderDayBefore;
-    }
-
-    public String getReminderTimeBefore() {
-        return reminderTimeBefore;
-    }
-
-    public void setReminderTimeBefore(String reminderTimeBefore) {
-        this.reminderTimeBefore = reminderTimeBefore;
-    }
 
     public String getRepeatDay() {
         return repeatDay;
