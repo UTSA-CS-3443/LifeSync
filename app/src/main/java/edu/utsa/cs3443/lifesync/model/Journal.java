59fe23b12406695b3ae9511571ec40160440e013
Jaime1108 <166854304+Jaime1108@users.noreply.github.com>
2024-07-24 19:49:25 -0500

    adding 4th screen

---
+++ app/src/main/java/edu/utsa/cs3443/lifesync/model/Journal.java
@@ -0,0 +1,10 @@
+package edu.utsa.cs3443.lifesync.model;
+
+public interface Journal {
+    String getName();
+    String getDescription();
+    String getDate();
+    String getLocation();
+    String getTime();
+
+}
---
+++ app/src/main/java/edu/utsa/cs3443/lifesync/model/Task.java
@@ -0,0 +1,84 @@
+package edu.utsa.cs3443.lifesync.model;
+
+import java.util.ArrayList;
+
+public class Task implements Journal{
+    private String name;
+    private ArrayList<String> guests;
+    private String description;
+    private String time;
+    private String date;
+    private String location;
+    private boolean repeat;
+
+    public Task(String name, String getName, String location, String date, String time, String description, ArrayList<String> guests, boolean repeat) {
+        this.name = name;
+        this.location = location;
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.guests = guests;
+        this.repeat = repeat;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getGuests() {
+        return guests;
+    }
+
+    public void setGuests(ArrayList<String> guests) {
+        this.guests = guests;
+    }
+
+    public boolean isRepeat() {
+        return repeat;
+    }
+
+    public void setRepeat(boolean repeat) {
+        this.repeat = repeat;
+    }
+}
