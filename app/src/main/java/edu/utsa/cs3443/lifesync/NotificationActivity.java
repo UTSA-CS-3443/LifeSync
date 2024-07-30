0922091fac55329f7a4ee271b3b31f8cf02fd8c3
Dylan Fernandez <68037135+Laforeze@users.noreply.github.com>
2024-07-30 13:08:07 -0500

    adding navbar to NotificationActivity.java

--- app/src/main/java/edu/utsa/cs3443/lifesync/NotificationActivity.java
+++ app/src/main/java/edu/utsa/cs3443/lifesync/NotificationActivity.java
@@ -1,6 +1,9 @@
 package edu.utsa.cs3443.lifesync;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,4 +29,54 @@ public class NotificationActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notifications);
         lvNotifications.setAdapter(adapter);
     }
+
+    public void createNavigationBar(){
+        ImageButton profile =findViewById(R.id.profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start ZoneActivity
+                Intent intent = new Intent(getBaseContext(), ProfileActivity.class);
+                intent.putExtra("user", user);
+                startActivity(intent);
+            }
+        });
+        ImageButton notification =findViewById(R.id.notification);
+        notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start ZoneActivity
+                Intent intent = new Intent(getBaseContext(), NotificationActivity.class);
+                intent.putExtra("user", user);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton create =findViewById(R.id.create);
+        create.setOnClickListener(v -> {
+            // Create an Intent to start ZoneActivity
+            Intent intent = new Intent(getBaseContext(), SecondActivity.class);
+            intent.putExtra("user", user);
+            startActivity(intent);
+        });
+
+        ImageButton home =findViewById(R.id.home);
+        home.setOnClickListener(v -> {
+            // Create an Intent to start ZoneActivity
+            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+            intent.putExtra("user", user);
+            startActivity(intent);
+        });
+        ImageButton calendar =findViewById(R.id.calendar);
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start ZoneActivity
+                Intent intent = new Intent(getBaseContext(), CalendarActivity.class);
+                intent.putExtra("user", user);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
